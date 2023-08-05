b600f5adb5e80224148f0568be65834c3e3a67f3
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-08-05 15:14:25 -0500

    adding a little bit of code

---
+++ R/initial playground.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(nhlapi)
+
+nhlapi::nhl_teams(1)
+get_team_id("Predators")
--- R/util_functions.R
+++ R/util_functions.R
@@ -3,3 +3,7 @@ get_team_id <- function(team_name) {
     dplyr::filter(grepl(team_name, name)) %$%
     id
 }
+
+get_team_games <- function(team_id) {
+
+}
